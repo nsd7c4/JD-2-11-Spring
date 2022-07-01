ffe465bcfcf5e31331b2a60e34af0791c286a9b2
rashidgafurov <nsd7c4@gmail.com>
2022-07-01 11:57:16 -0400

    Initial commit

---
+++ src/main/java/com/grr/jd211spring/Jd211SpringApplication.java
@@ -0,0 +1,13 @@
+package com.grr.jd211spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jd211SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jd211SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grr/jd211spring/Jd211SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grr.jd211spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jd211SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
